014ef6c024d547dd654194fc42f0b7d841112a3f
alkanoic <alkanoic@gmail.com>
2023-01-29 23:12:34 +0900

    add docker compose restart always setting

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
-docker run -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:20.0.3 start-dev
+docker run -d -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin --name keycloak quay.io/keycloak/keycloak:latest start-dev
+
+# http://localhost:8080
